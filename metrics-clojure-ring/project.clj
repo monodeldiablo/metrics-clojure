98be7c079567fbc13d29e8a4ef24648d7607501c
Brian Davis <bdavis@climate.com>
2012-08-08 12:04:50 +0200

    Updated the ring wrapper to depend on my fork

--- metrics-clojure-ring/project.clj
+++ metrics-clojure-ring/project.clj
@@ -1,4 +1,4 @@
 (defproject metrics-clojure-ring "0.8.0"
   :description "Various things gluing together metrics-clojure and ring."
   :dependencies [[cheshire "2.2.2"]
-                 [metrics-clojure "0.8.0"]])
+                 [org.clojars.monodeldiablo/metrics-clojure "0.8.0"]])
